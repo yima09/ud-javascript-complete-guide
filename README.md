4ad18fb79862cfeda5e18f72a4a91f72d41281d9
yima09 <u.41383152@gmail.com>
2025-04-03 08:32:14 +0900

    25. typeof 演算子、データの型を表示

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # ud-javascript-complete-guide
 
 
-### +=や++などの省略記法を使う方法
+### 16. +=や++などの省略記法を使う方法
 
 [main01.js](basic/main01.js)
 
-### 明示的に型を変換する方法
+### 22. 明示的に型を変換する方法
 
+[main02.js](basic/main02.js)
 
+### 25. typeof 演算子、データの型を表示
 
+[main03.js](basic/main03.js)
 
 
 
---
+++ basic/main03.js
@@ -0,0 +1,10 @@
+// typeof 演算子、データの型を表示
+console.log(typeof 3); // number
+console.log(typeof 3.2); // number
+console.log(typeof NaN); // number
+console.log(typeof true); // boolean
+console.log(typeof 'Hello'); // string
+console.log(typeof { name: 'ken' }); // object
+console.log(typeof [1, 2, 3]); // object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

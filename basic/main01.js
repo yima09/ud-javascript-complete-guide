2ce729812e00cdeb58a04c65ebf577a57fd9d46f
yima09 <u.41383152@gmail.com>
2025-04-03 08:13:08 +0900

    16. += や ++ などの省略記法を使う方法

---
+++ basic/main01.js
@@ -0,0 +1,9 @@
+// += や ++ などの省略記法を使う方法
+let result = 5;
+console.log(result += 1); // 6
+console.log(result); // 6
+result++; // 7
+console.log(result++); // 7 表示した後に + 1
+console.log(result); // 8
+console.log(++result); // 9 にした後に表示
+console.log(result); // 9

cab7e337c48db5ea9e775f8729ed41fbbc08c597
yima09 <u.41383152@gmail.com>
2025-04-06 04:23:53 +0900

    118. スプレッド構文でオブジェクトをコピー

---
+++ object/main03.js
@@ -0,0 +1,27 @@
+'use strict';
+
+{
+  const coffee = {
+    name: 'Espresso',
+    size: 350,
+    nutritions: {
+      calories: 5,
+      sugars: 0,
+    },
+  };
+
+  const coffee2 = coffee;
+  console.log(coffee2 === coffee, coffee2.nutritions === coffee.nutritions); // true true
+
+  const coffee3 = { ...coffee };
+  console.log(coffee3 === coffee, coffee3.nutritions === coffee.nutritions); // false true 中のオブジェクトがシャローコピー。
+
+  const coffee4 = {
+    ...coffee,
+    nutritions: { ...coffee.nutritions },
+  };
+  console.log(coffee4 === coffee, coffee4.nutritions === coffee.nutritions); // false false
+
+  console.log(coffee3); // 見た感じのオブジェクトは同じ。
+  console.log(coffee4);
+}

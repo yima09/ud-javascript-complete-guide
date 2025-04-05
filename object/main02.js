ec000d67f6cf760f61a92169a2f0d1f49e6f81eb
yima09 <u.41383152@gmail.com>
2025-04-06 04:18:13 +0900

    117. プロパティ省略記法

--- object/main02.js
+++ object/main02.js
@@ -1,27 +1,13 @@
 'use strict';
 
 {
+  const name = 'Espresso';
+  const size = 350;
   const coffee = {
-    name: 'Espresso',
-    size: 350,
-    nutritions: {
-      calories: 5,
-      sugars: 0,
-    },
+    name: name,
+    size, // 省略記法。
   };
 
-  const coffee2 = coffee;
-  console.log(coffee2 === coffee, coffee2.nutritions === coffee.nutritions); // true true
-
-  const coffee3 = { ...coffee };
-  console.log(coffee3 === coffee, coffee3.nutritions === coffee.nutritions); // false true
-
-  const coffee4 = {
-    ...coffee,
-    nutritions: { ...coffee.nutritions },
-  };
-  console.log(coffee4 === coffee, coffee4.nutritions === coffee.nutritions); // false false
-
-  console.log(coffee3);
-  console.log(coffee4);
+  console.log(coffee.name); // Espresso
+  console.log(coffee.size); // 350
 }
